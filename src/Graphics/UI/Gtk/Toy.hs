68cc3c3dbff10750f962f2cbe562dc9890f38fc9
Michael Sloan <mgsloan@lappy.(none)>
2012-01-16 21:42:47 -0800

    Haddock fixes, type synonyms, and API changes

--- src/Graphics/UI/Gtk/Toy.hs
+++ src/Graphics/UI/Gtk/Toy.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections
+           , UndecidableInstances
+           , ViewPatterns #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Graphics.UI.Gtk.Toy
@@ -8,21 +10,21 @@
 -- Stability   :  experimental
 -- Portability :  GHC only
 --
--- The Gtk Toy Framework is a simplifying interface over Gtk for creating
--- applications which draw things and interact with the mouse and keyboard.  It
--- handles the minutae of setting up the Gtk window and canvas, and processes
--- the input events into more palatable data structures.
+-- The Gtk Toy Framework is an interface over Gtk for creating applications
+-- which draw things and use the mouse or keyboard.
+--
+-- It handles the minutiae of setting up the Gtk window and canvas, and
+-- processes mouse and keyboard inputs into more palatable data structures.
 -----------------------------------------------------------------------------
 module Graphics.UI.Gtk.Toy
   (
     KeyInfo, KeyTable, InputState(..), Interactive(..)
   , runToy, quitToy
 
-  -- * InputState Accessors.
-  , keyInfo, keyHeld, mousePos, mouseHeld
+  -- * InputState Accessors
+  , keyInfo, keyHeld, mouseHeld
 
-  -- * Utilities for writing Interactive instances.
-
+  -- * Utilities
   -- | Functions to allow for writing simpler, pure implementations of the
   --   different members of Interactive.
   , simpleTick, simpleDisplay, simpleMouse, simpleKeyboard
@@ -38,10 +40,30 @@ import qualified Data.Map as M
 import qualified Graphics.UI.Gtk as G
 import qualified Graphics.UI.Gtk.Gdk.Events as E
 
+-- | Information about the most recent key-state transition.
+--   The tuple contains whether the button was pressed, @True@, or released, @False,
+--   at what time in msec, with which GTK modifiers.
 type KeyInfo = (Bool, Int, [G.Modifier])
+
+-- | A map of GTK keynames to last-received event regarding each respective
+--   key.  This can be interpreted as the current keyboard state - a key is
+--   down if it was last seen being pressed.
 type KeyTable = M.Map String KeyInfo
 
-data InputState = InputState (Double, Double) KeyTable
+data InputState = InputState 
+  { mousePos :: (Double, Double) -- ^ The most recent mouse position
+  , keyTable :: KeyTable         -- ^ Map from key-name to most recent event
+  }
+
+-- | A KeyEvent tuple specifies whether the key was pressed or not, and
+--   which key was pressed.  @(Right Char)@ is yielded for keys which would
+--   normally correspond to character insertions, while @(Left String) provides
+--   GTK-convention names for the rest.
+type KeyEvent = (Bool, Either String Char)
+
+-- | A MouseEvent is @Nothing@ if it's a mouse motion event, and otherwise
+--   provides mouse press information.
+type MouseEvent = Maybe (Bool, Int)
 
 -- | A class for things which can be drawn and change within an interactive
 --   context.  The default method implementations do nothing.
@@ -49,57 +71,43 @@ class Interactive a where
 
   -- | @tick@ is (ideally) called every 30ms.  The bool result indicates if the
   --   graphics need to be refreshed.
-  tick     :: InputState -> a -> IO (a, Bool)
+  tick                     :: InputState -> a -> IO (a, Bool)
 
   -- | @display@ is called when the rendering needs to be refreshed.
-  display  :: G.DrawWindow 
-           -> InputState -> a -> IO a
+  display  :: G.DrawWindow -> InputState -> a -> IO a
 
   -- | @mouse@ is called when the mouse moves or presses occur.
-  mouse    :: Maybe (Bool, Int)
-           -> InputState -> a -> IO a
+  mouse    :: MouseEvent   -> InputState -> a -> IO a
 
-  -- | @keyboard@ is called on key-presses, first parameter is "pressed?", and 
-  --   second is (Right Char) for string-representeable names, and (Left String)
-  --   for the rest.
-  keyboard :: Bool -> Either String Char 
-           -> InputState -> a -> IO a
+  -- | @keyboard@ is called on key-presses.  
+  keyboard :: KeyEvent     -> InputState -> a -> IO a
 
   -- No-op defaults.
   tick _ = return . (, False)
-  mouse    = simpleMouse    (const $ const id)
-  keyboard = simpleKeyboard (const $ const id)
+  display  _ _ = return
+  mouse    _ _ = return
+  keyboard _ _ = return
 
 -- InputState Queries.
 
--- | Gets the information for the most recent key press / release of the named
--- key.  If the key is representeable as a Char, then the lowercase variant is
--- its name.  Otherwise, it's string form name is the identifier.
+-- | Gets the information for the most recent key event of the named key.  
 keyInfo :: String -> InputState -> Maybe KeyInfo
-keyInfo name (InputState _ kbd)
-  | length name == 1 = M.lookup (map toLower name) kbd
-  | otherwise = M.lookup name kbd
+keyInfo name = M.lookup name . keyTable
 
 -- | Gets whether the named key is currently thought to be held down.
 keyHeld :: String -> InputState -> Bool
-keyHeld name inp = case keyInfo name inp of
-  (Just (True, _, _)) -> True
-  _ -> False
+keyHeld name (keyInfo name -> Just (True, _, _)) = True
+keyHeld _ _ = False
 
 -- | Postfixes "_L" and "_R" on the key name, and returns true if either of
 --   those keys are being held. 
 eitherHeld :: String -> InputState -> Bool
 eitherHeld key inp = (keyHeld (key ++ "_L") inp || keyHeld (key ++ "_R") inp)
 
--- | Accesses the most recently reported mouse position.
-mousePos :: InputState -> (Double, Double)
-mousePos (InputState p _) = p
-
--- | Queries if a particular mouse button was reported as held.
+-- | Yields whether the mouse button (1st parameter) is considered pressed
+--   in the passed InputState.
 mouseHeld :: Int -> InputState -> Bool
 mouseHeld ix = keyHeld ("Mouse" ++ show ix)
-
--- Convenience functions to use pure functions that don't need the InputState.
 
 -- | Converts a pure state transform to a function for Interactive 'tick'.
 simpleTick :: (a -> a)
@@ -112,36 +120,33 @@ simpleDisplay :: (G.DrawWindow -> a -> a)
 simpleDisplay f dw _ = return . f dw
 
 -- | Converts a function which responds to mouse-presses, and transforms state
--- accordingly to a function for Interactive 'mouse'.
-simpleMouse :: ((Double, Double) -> Maybe (Bool, Int) -> a -> a)
-            -> Maybe (Bool, Int)
-            -> InputState -> a -> IO a
-simpleMouse f c inp = return . f (mousePos inp) c
+--   accordingly to a function for Interactive 'mouse'.
+simpleMouse :: (MouseEvent -> (Double, Double) -> a -> a)
+            -> (MouseEvent -> InputState -> a -> IO a)
+simpleMouse f c inp = return . f c (mousePos inp)
 
 -- | Converts a function which responds to mouse-presses, and transforms state
--- accordingly to a function for Interactive 'mouse'.
-simpleMouseClick :: ((Double, Double) -> (Bool, Int) -> a -> a)
-                 -> Maybe (Bool, Int)
-                 -> InputState -> a -> IO a
-simpleMouseClick f (Just c) inp = return . f (mousePos inp) c
+--   accordingly to a function for Interactive 'mouse'.
+simpleMouseClick :: ((Bool, Int) -> (Double, Double) -> a -> a)
+                 -> (MouseEvent -> InputState -> a -> IO a)
+simpleMouseClick f (Just c) inp = return . f c (mousePos inp)
 simpleMouseClick _ _ _ = return
 
 simpleMousePos :: ((Double, Double) -> a -> a)
-               -> Maybe (Bool, Int)
-               -> InputState -> a -> IO a
+               -> (MouseEvent -> InputState -> a -> IO a)
 simpleMousePos f _ inp = return . f (mousePos inp)
 
 -- | Converts a function which responds to key-presses, and transforms state
 --   accordingly to a function for Interactive 'keyboard'.
-simpleKeyboard :: (Bool -> Either String Char -> a -> a)
-               -> (Bool -> Either String Char -> InputState -> a -> IO a)
-simpleKeyboard f p k _ = return . f p k
+simpleKeyboard :: (KeyEvent -> a -> a)
+               -> (KeyEvent -> InputState -> a -> IO a)
+simpleKeyboard f e _ = return . f e
 
 -- | A definition for the keyboard handler that just calls "quitToy" when
 --   Escape is pressed.
-quitKeyboard :: Bool -> Either String Char -> InputState -> a -> IO a
-quitKeyboard True (Left "Escape") _ x = quitToy >> return x
-quitKeyboard _ _ _ x = return x
+quitKeyboard :: KeyEvent -> InputState -> a -> IO a
+quitKeyboard (True, (Left "Escape")) _ x = quitToy >> return x
+quitKeyboard _ _ x = return x
 
 -- | Like it says on the can.  This is a synonym for 'Graphics.UI.Gtk.mainQuit'
 quitToy :: IO ()
@@ -194,7 +199,7 @@ handleKey :: Interactive a => IORef (InputState, a) -> E.Event -> IO ()
 handleKey st ev = do
   (InputState p m, x) <- readIORef st
   let inp' = InputState p (M.insert name (pres, time, mods) m)
-  x' <- keyboard pres (maybe (Left name) Right char) inp' x
+  x' <- keyboard (pres, maybe (Left name) Right char) inp' x
   writeIORef st (inp', x')
  where
   name = E.eventKeyName ev
